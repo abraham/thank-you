69bc503f9d7f2c259419084047ad9de69bfda153
Abraham Williams <4braham@gmail.com>
2017-01-29 14:39:18 -0600

    Added addition form fields and use params.require

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,11 +1,17 @@
 class ActionsController < ApplicationController
   def create
-    render plain: params.inspect
+    @action = Action.create(action_params)
+
+    redirect_to @action
   end
 
-  def index
-  end
+  def index; end
 
-  def new
+  def new; end
+
+  private
+
+  def action_params
+    params.require(:action).permit(:status_id, :text)
   end
 end
