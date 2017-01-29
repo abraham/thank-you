c0ea6c4cc86bb1be4feab3047a32a2a3d63d6e76
Abraham Williams <4braham@gmail.com>
2017-01-29 16:39:14 -0600

    Update statuses routes to tweets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'statuses/new'
-  post 'statuses/create'
+  get 'tweets/new'
+  post 'tweets/create'
   resources :thanks
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
