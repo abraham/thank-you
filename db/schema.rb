7868cfffb7503df8ed0adad4726ee3a15ec24929
Abraham Williams <4braham@gmail.com>
2017-01-30 12:34:40 -0600

    Added faker gem

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170129195928) do
 
   create_table "thanks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "text", null: false
-    t.string "in_reply_to_status_id", null: false
+    t.string "in_reply_to_status_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
