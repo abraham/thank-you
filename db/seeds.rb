154b7cef7307e0da5e2c7cad05af5d8730b23257
Abraham Williams <4braham@gmail.com>
2017-01-30 12:41:45 -0600

    Added initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |i|
+  Thank.create(text: "Thank you @#{Faker::Internet.user_name} for #{Faker::Hipster.sentence}")
+end
