3020f10126998450f0899d55733204e8049dfdfd
Abraham Williams <4braham@gmail.com>
2017-01-30 18:43:13 -0600

    Create inital stage of twitter flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
   def new
-    # Create request_token
-    # Save request_token to db
-    # Set request_token to sessions
-    # Redirect to twitter
+    twitter_request_token = consumer.get_request_token
+    request_token = RequestToken.create(token: twitter_request_token.token,
+                                        secret: twitter_request_token.secret)
+    cookies[:request_token_id] = request_token.id
+
+    redirect_to twitter_request_token.authorize_url
   end
 
   def create
@@ -19,5 +21,14 @@ class SessionsController < ApplicationController
   def destroy
     # destroy user session
     # redirect to homepage
+  end
+
+  private
+
+  def consumer
+    OAuth::Consumer.new(Rails.application.secrets.twitter_consumer_key,
+                        Rails.application.secrets.twitter_consumer_secret,
+                        site: 'https://api.twitter.com',
+                        scheme: :header)
   end
 end
