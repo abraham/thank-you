0f4aee0ab757b029c15ed87e0dd6bcd7145dacce
Abraham Williams <4braham@gmail.com>
2017-02-01 16:32:48 -0600

    Make current_user available in views, improve find logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
   def current_user
-    User.find(cookies[:user_id])
+    @current_user ||= User.find_by(id: cookies[:user_id])
   end
 end
