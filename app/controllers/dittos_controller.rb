934ee8ccba9332f0bbdc567e7a5aaa42d33de7d6
Abraham Williams <4braham@gmail.com>
2017-02-01 15:35:27 -0600

    Save user when creating ditto

--- app/controllers/dittos_controller.rb
+++ app/controllers/dittos_controller.rb
@@ -3,10 +3,12 @@ class DittosController < ApplicationController
     thank = Thank.find(params[:id])
     ditto = Ditto.new(dittos_params)
     ditto.thank = thank
+    ditto.user = current_user
     tweet = current_user.tweet(ditto.text, thank.reply_to_tweet_id)
     ditto.tweet_id = tweet.id
     ditto.data = tweet.to_hash
     ditto.save
+    # TODO: check for success and add flash
 
     redirect_to thanks_show_path(thank)
   end
