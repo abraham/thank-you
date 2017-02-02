b15dff7c365a7c1e19d9dcbaef7ee9e1df74d731
Abraham Williams <4braham@gmail.com>
2017-02-01 20:41:02 -0600

    Added require_signin method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: cookies[:user_id])
   end
+
+  def require_signin
+    return if current_user
+
+    redirect_to sessions_new_path
+  end
 end
