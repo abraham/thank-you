9353f112f1e198ff281952695ef2d306111eca06
Abraham Williams <4braham@gmail.com>
2017-02-01 18:23:59 -0600

    require factory_girl

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,9 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'webmock/minitest'
+require 'factory_girl'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
   # Add more helper methods to be used by all tests here...
+  include FactoryGirl::Syntax::Methods
 end
