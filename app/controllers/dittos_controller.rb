ab1de18284a73d68ba950534e789cf5ae9e69933
Abraham Williams <4braham@gmail.com>
2017-02-01 23:14:26 -0600

    Catch errors posting to Twitter

--- app/controllers/dittos_controller.rb
+++ app/controllers/dittos_controller.rb
@@ -6,7 +6,13 @@ class DittosController < ApplicationController
     ditto = Ditto.new(dittos_params)
     ditto.thank = thank
     ditto.user = current_user
-    tweet = current_user.tweet(ditto.text, thank.reply_to_tweet_id)
+    begin
+      tweet = current_user.tweet(ditto.text, thank.reply_to_tweet_id)
+    rescue Twitter::Error::Forbidden => error
+      logger.error "Error posting Ditto to Twitter Code: #{error.code} Message: #{error.message}"
+      flash[:error] = "Something went wrong posting to Twitter. Code: #{error.code} - #{error.message}"
+      redirect_to thanks_show_path(thank) and return
+    end
     ditto.tweet_id = tweet.id
     ditto.data = tweet.to_hash
 
