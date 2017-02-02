bff6e21e171deea602c44ab4fab722c147629950
Abraham Williams <4braham@gmail.com>
2017-02-02 10:57:40 -0600

    Test User.admin?

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @default_admin_twitter_ids = AppConfig.admin_twitter_ids
+  end
+
+  def teardown
+    AppConfig.admin_twitter_ids = @default_admin_twitter_ids
+  end
+
+  test 'admin? works for admins' do
+    user = create(:user)
+    AppConfig.admin_twitter_ids = [user.twitter_id]
+    assert user.admin?
+  end
+
+  test 'admin? does not work for users' do
+    user = create(:user)
+    assert !user.admin?
+  end
 end
