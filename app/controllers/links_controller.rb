f6a9c38f596ef42500d43c5c877270d80f19bf86
Abraham Williams <4braham@gmail.com>
2017-02-03 15:25:57 -0600

    Add find_thank before_action to LinksController

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,27 +1,30 @@
 class LinksController < ApplicationController
   before_action :require_signin
+  before_action :find_thank, only: [:create, :new]
 
   def create
-    thank = Thank.find(params[:thank_id])
     link = Link.new(links_params)
-    link.thank = thank
+    link.thank = @thank
     link.user = current_user
 
     if link.save
       flash[:notice] = 'Citation was successfully created.'
-      redirect_to thank_path(thank)
+      redirect_to thank_path(@thank)
     else
       render 'new'
     end
   end
 
   def new
-    @thank = Thank.find(params[:thank_id])
   end
 
   private
 
   def links_params
     params.require(:link).permit(:text, :url)
+  end
+
+  def find_thank
+    @thank = Thank.find(params[:thank_id])
   end
 end
