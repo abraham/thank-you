6271b01678dfdcfca97b9e8abc5f9c73e65cb552
Abraham Williams <4braham@gmail.com>
2017-02-03 09:18:56 -0600

    Added dittos_count to Thanks

---
+++ db/migrate/20170203151653_add_dittos_count_to_thanks.rb
@@ -0,0 +1,5 @@
+class AddDittosCountToThanks < ActiveRecord::Migration[5.1]
+  def change
+    add_column :thanks, :dittos_count, :integer, null: false, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202224514) do
+ActiveRecord::Schema.define(version: 20170203151653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170202224514) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.uuid "user_id", null: false
+    t.integer "dittos_count", default: 0, null: false
     t.index ["user_id"], name: "index_thanks_on_user_id", using: :btree
   end
 
