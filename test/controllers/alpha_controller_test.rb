984bf6b3ac42d5eebc32a90059c23ca7b1db3c99
Abraham Williams <4braham@gmail.com>
2017-02-03 18:59:44 -0600

    Added AlphaControler route test

--- test/controllers/alpha_controller_test.rb
+++ test/controllers/alpha_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class AlphaControllerTest < ActionDispatch::IntegrationTest
+  test 'GET /alpha/join/123' do
+    assert_routing({ path: 'alpha/join/123', method: :get }, { controller: 'alpha', action: 'join', token: '123' })
+  end
+
   test 'should redirect to root' do
     get alpha_join_url token: 'secure-token'
     assert_redirected_to root_url
