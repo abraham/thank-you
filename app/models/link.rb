0c5626ee2ac081bb69cd60849622ab68341bf82d
Abraham Williams <4braham@gmail.com>
2017-02-09 14:33:11 -0600

    test Link url protocol

--- app/models/link.rb
+++ app/models/link.rb
@@ -6,4 +6,12 @@ class Link < ApplicationRecord
   validates :user, presence: true
   validates :text, presence: true
   validates :url, presence: true
+  validate :url_protocol
+
+  private
+
+  def url_protocol
+    return if url.starts_with?('http://') || url.starts_with?('https://')
+    errors.add(:url, 'must be a valid URL')
+  end
 end
--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'url has a protocol' do
+    deed = create(:deed)
+    user = create(:user)
+    link = Link.new(text: Faker::Lorem.word, url: 'example.com', user: user, deed: deed)
+    assert_not link.valid?
+  end
 end
