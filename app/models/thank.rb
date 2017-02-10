43ee90723e58ebbbcee46fb14eb99310940f3b19
Abraham Williams <4braham@gmail.com>
2017-02-10 14:42:36 -0600

    Test Thank.text length

--- app/models/thank.rb
+++ app/models/thank.rb
@@ -1,11 +1,20 @@
 class Thank < ApplicationRecord
+  include Twitter::Validation
+
   belongs_to :deed, counter_cache: true
   belongs_to :user, counter_cache: true
 
+  validate :text_length
   validates :deed_id, uniqueness: { scope: :user_id, message: 'you has already been given' }
   validates :deed, presence: true
   validates :text, presence: true
   validates :user, presence: true
 
   default_scope { order(created_at: :desc) }
+
+  private
+
+  def text_length
+    errors.add(:text, 'is not a valid tweet') if tweet_invalid?(text)
+  end
 end
--- test/models/thank_test.rb
+++ test/models/thank_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ThankTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'text length conforms to Twitter spec' do
+    deed = create(:deed)
+    thank = deed.thanks.new(user: deed.user, text: 'Hello World')
+    assert thank.valid?
+    thank.text = 'n' * 200
+    assert !thank.valid?
+    assert_equal 'Text is not a valid tweet', thank.errors.full_messages.first
+    thank.text = 'https://example.com ' * 6
+    assert !thank.valid?
+    assert_equal 'Text is not a valid tweet', thank.errors.full_messages.first
+    thank.text = "Thank You @twitter for #{'n' * 93} https://example.com"
+    assert thank.valid?
+    thank.text = "Thank You @twitter for #{'n' * 94} https://example.com"
+    assert !thank.valid?
+    assert_equal 'Text is not a valid tweet', thank.errors.full_messages.first
+  end
 end
