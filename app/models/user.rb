dde54ac9bebe70cdf528662561655d64677c2924
Abraham Williams <4braham@gmail.com>
2017-02-11 07:41:02 -0600

    Remove duplicate user client

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,16 +17,7 @@ class User < ApplicationRecord
   end
 
   def tweet(text, in_reply_to_status_id)
-    twitter_client.update(text, in_reply_to_status_id: in_reply_to_status_id) if AppConfig.posting_to_twitter_enabled
-  end
-
-  def twitter_client
-    Twitter::REST::Client.new do |config|
-      config.consumer_key = Rails.application.secrets.twitter_consumer_key
-      config.consumer_secret = Rails.application.secrets.twitter_consumer_secret
-      config.access_token = access_token
-      config.access_token_secret = access_token_secret
-    end
+    client.update(text, in_reply_to_status_id: in_reply_to_status_id) if AppConfig.posting_to_twitter_enabled
   end
 
   def admin?
