dd40ee2f704db2e21ac55c269f6baf0edce9d12b
Abraham Williams <4braham@gmail.com>
2017-02-13 10:58:50 -0600

    Require MDC

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require material-components-web/dist/material-components-web
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+mdc.autoInit();
