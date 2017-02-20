5d19b898fa4badb7d2b8e11036fa413ee4bc35be
Abraham Williams <4braham@gmail.com>
2017-02-20 16:23:29 -0600

    Test that tweets get rendered when present

---
+++ test/integration/tweet_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class TweetTest < ActionDispatch::IntegrationTest
+  test '#show should not include tweet card' do
+    deed = create(:deed)
+    get deed_url(deed)
+    assert_response :success
+    assert_select '.tweet', false
+    assert_nil deed.twitter_id
+    assert_nil deed.data
+  end
+
+  test '#show should include tweet card' do
+    deed = create(:deed, :with_tweet)
+    get deed_url(deed)
+    assert_response :success
+    assert deed.twitter_id
+    assert deed.data
+    assert_select '.tweet', 1
+    assert_select ".tweet-#{deed.twitter_id}" do
+      assert_select '.mdc-card__media', 0
+      assert_select 'style', 0
+      assert_select 'div.mdc-card__horizontal-block' do
+        assert_select "a[href=\"#{deed.tweet.user.uri}\"]" do
+          assert_select "img[src=\"#{deed.tweet.user.profile_image_uri_https(:bigger)}\"].avatar", 1
+        end
+        assert_select 'section.mdc-card__primary' do
+          assert_select "div.mdc-card__title a[href=\"#{deed.tweet.user.uri}\"]", deed.tweet.user.name
+          assert_select "div.mdc-card__subtitle a[href=\"#{deed.tweet.user.uri}\"]", "@#{deed.tweet.user.screen_name}"
+        end
+      end
+      assert_select 'section.mdc-card__supporting-text', deed.tweet.full_text
+      assert_select 'section.mdc-card__supporting-text' do
+        assert_select "a[href=\"#{deed.tweet.uri}\"]", deed.tweet.created_at.strftime('%l:%M %p - %e %b %Y')
+      end
+    end
+  end
+end
