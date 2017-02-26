27e89b0e18a06f87122e827235748f3d8c95a73a
Abraham Williams <4braham@gmail.com>
2017-02-25 18:00:07 -0600

    Added user factory traits

--- test/factories/user.rb
+++ test/factories/user.rb
@@ -1,10 +1,8 @@
 FactoryGirl.define do
   factory :user do
     status :active
-    data do
-      Faker::Twitter.user.merge(email: Faker::Internet.safe_email,
-                                screen_name: Faker::Internet.user_name(nil, ['_']))
-    end
+    role :user
+    data { Faker::Twitter.user(include_email: true) }
     twitter_id { data[:id].to_s }
     screen_name { data[:screen_name] }
     name { data[:name] }
@@ -13,5 +11,25 @@ FactoryGirl.define do
     access_token { "#{twitter_id}-#{Faker::Internet.password(30, 40)}" }
     access_token_secret { Faker::Internet.password(40, 50) }
     default_avatar { data[:default_profile_image] }
+
+    trait :editor do
+      role :editor
+    end
+
+    trait :moderator do
+      role :moderator
+    end
+
+    trait :admin do
+      role :admin
+    end
+
+    trait :disabled do
+      status :disabled
+    end
+
+    trait :expired do
+      status :expired
+    end
   end
 end
