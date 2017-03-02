b2a75c7c2ea3aa1e518509459b96d1bb580ebaaf
Abraham Williams <4braham@gmail.com>
2017-03-01 18:54:01 -0600

    Fix menu position on header

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,5 +1,5 @@
 document.addEventListener('turbolinks:load', function() {
-  var menuEl = document.querySelector('.mdc-simple-menu');
+  var menuEl = document.querySelector('header .mdc-simple-menu');
   var menuIconEl = document.querySelector('header .menu')
   if (menuIconEl && menuEl) {
     var menu = new mdc.menu.MDCSimpleMenu(menuEl);
