012044b917f0a07e49831676e1a6377612d76e49
Abraham Williams <4braham@gmail.com>
2017-03-08 21:25:23 -0600

    Added list of topics and option to join test topic

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,27 @@
 class NotificationsController < ApplicationController
+  before_action :enable_test
+
   def index
+    @topics = [
+      {
+        id: :deeds,
+        text: 'Tell me about new Deeds'
+      }
+    ]
+
+    return unless (current_user && current_user.admin?) || @test_push
+
+    @topics << {
+      id: :test,
+      text: 'Tell me about test notifications'
+    }
+  end
+
+  private
+
+  def enable_test
+    cookies[:test_push] = true if params[:test] == 'push'
+
+    @test_push = cookies[:test_push]
   end
 end
